ca6e9d9a656ca00653e2059ceae8e82abc961541
Marco Martinez <marco.antoni.martinez@gmail.com>
2025-07-30 16:39:45 +0000

    Finished coding api call

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+
+gem "dotenv"
+gem "ruby-openai"
+
+
+retrieved_response = client.responses.retrieve(response_id: response["id"])
+puts retrieved_response["object"]
--- chat.rb
+++ chat.rb
@@ -1,1 +1,44 @@
-# Write your solution here!
+
+require "openai"
+require "dotenv/load"
+
+client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+
+# Prepare an Array of previous messages
+message_list = [
+  {
+    "role" => "system",
+    "content" => "You are a helpful assistant."
+  }
+]
+
+input = " "
+
+while input != "bye"
+
+  puts "Hello! How can I help you today?"
+  puts "__________________________________________________"
+
+  input = gets.chomp
+
+  if input != "bye"
+    message_list.push({"role" => "user", "content" => input})
+
+    # Call the API to get the next message from GPT
+    api_response = client.chat(
+      parameters: {
+       model: "gpt-3.5-turbo",
+       messages: message_list
+     }
+    )
+
+    puts api_response.fetch("choices")[0].fetch("message").fetch("content")
+
+    message_list.push({"role" => "assistant", "content" => api_response.fetch("choices")[0].fetch("message").fetch("content")})
+
+    puts "_"*50
+  end
+
+end
+
+puts "Goodbye! Have a great day!"
